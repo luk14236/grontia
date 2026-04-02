0e7c36f36d49f3680b0dbe4750b5a9d879638b68
Lucas Oliveira <luk14236@gmail.com>
2026-04-02 21:46:20 +0200

    main resolved

--- infrastructure/terraform/envs/prod/main.tf
+++ infrastructure/terraform/envs/prod/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "dbw-grondia-dev"
+    resource_group_name  = "rg-grondia-dev"
     storage_account_name = "stgrondiadev12345"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
